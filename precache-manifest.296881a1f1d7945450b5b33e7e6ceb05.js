399dfc92f05c200fe6f2f068fbe7725693dc5712
nyanyajjang <woocowoo99@daum.net>
2020-05-28 13:27:07 -0400

    Updates

---
+++ precache-manifest.296881a1f1d7945450b5b33e7e6ceb05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f9d40a3737039920006420f760e656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc4669efd91f8cf746f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8197ba6d098213a1e69c",
+    "url": "/static/js/2.ecc0969e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ecc0969e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc4669efd91f8cf746f",
+    "url": "/static/js/main.afeeed9b.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
