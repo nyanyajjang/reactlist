7a8538c88fc9d5cd7443a567d4a10e527288fa15
nyanyajjang <woocowoo99@daum.net>
2020-05-28 16:24:19 -0400

    Updates

--- precache-manifest.4c9e0e47a62dd749abbd526db68ecfa6.js
+++ precache-manifest.4c9e0e47a62dd749abbd526db68ecfa6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f9959b0eb6d1040c867f84f1e17e245",
+    "revision": "fcef908d17ee9deeb17e42b4373ab65e",
     "url": "/reactlist/index.html"
   },
   {
-    "revision": "8c2c118dbaa1fbb822b2",
+    "revision": "f9b6799ce2c8d938ad59",
     "url": "/reactlist/static/css/main.fb5e79b7.chunk.css"
   },
   {
-    "revision": "766105077e17fae79c86",
-    "url": "/reactlist/static/js/2.120c559b.chunk.js"
+    "revision": "4570dd17b253ab801000",
+    "url": "/reactlist/static/js/2.222c9ce6.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/reactlist/static/js/2.120c559b.chunk.js.LICENSE.txt"
+    "url": "/reactlist/static/js/2.222c9ce6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8c2c118dbaa1fbb822b2",
-    "url": "/reactlist/static/js/main.357cafea.chunk.js"
+    "revision": "f9b6799ce2c8d938ad59",
+    "url": "/reactlist/static/js/main.c070aa6a.chunk.js"
   },
   {
     "revision": "d3be93c814dcda2f8a27",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactlist/precache-manifest.ec88dff92c8d63805c00e99282c7fc45.js"
+  "/reactlist/precache-manifest.4c9e0e47a62dd749abbd526db68ecfa6.js"
 );
 
 self.addEventListener('message', (event) => {
